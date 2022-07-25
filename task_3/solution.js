d37f68edc4d82d98228eb95362d5484f2c033334
Aleksey18008 <aleksey18008@gmail.com>
2022-07-25 16:24:13 +0300

    update task №3 to one line

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,17 +14,20 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    let itemCountText = ""; // 1. Создайте переменную itemCountText, равную пустой строке
+
+    // 1. Создайте переменную itemCountText, равную пустой строке
     
-    itemCountText += product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText += ` × `; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText += product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText += ` ₽ = `; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     
-    let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     
-    itemCountText += sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    itemCountText += ` ₽`; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${product.count * product.price} ₽`;   
 
     // Конец решения задания №3.1.
 
